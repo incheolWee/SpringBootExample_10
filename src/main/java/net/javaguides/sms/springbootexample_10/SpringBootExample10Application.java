3e042f06383ca8f3feecb57a2d44055323028307
dnldl <dnldlscjf@gmail.com>
2024-02-28 18:39:34 +0900

    기본적인 구조 완성

---
+++ src/main/java/net/javaguides/sms/springbootexample_10/SpringBootExample10Application.java
@@ -0,0 +1,13 @@
+package net.javaguides.sms.springbootexample_10;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootExample10Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootExample10Application.class, args);
+	}
+
+}
---
+++ src/test/java/net/javaguides/sms/springbootexample_10/SpringBootExample10ApplicationTests.java
@@ -0,0 +1,13 @@
+package net.javaguides.sms.springbootexample_10;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootExample10ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
